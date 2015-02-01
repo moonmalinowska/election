136022ee5c4d97edba57a7ae4004a82faa340c21
moonmalinowska <moon.malinowska@student.uj.edu.pl>
2015-02-01 22:52:59 +0100

    aa 1.1

--- election/app/models/user.rb
+++ election/app/models/user.rb
@@ -13,6 +13,14 @@ class User < ActiveRecord::Base
         message: "Ilość znaków loginu: 6-15"
     }
 
+=begin
+  acts_as_authentic do |config|
+    config.validate_login_field = false
+    config.validate_password_field = false
+    config.encrypted_password_field = :crypted_password
+    config.require_password_confirmation = true
+  end
+=end
   ROLES = %w[admin centralny okręgowy]
   def role?(base_role)
     ROLES.index(base_role.to_s) <= ROLES.index(role)
